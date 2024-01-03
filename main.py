032f7b356ecc7a96b9e1ff174474b5c486185e56
Sakhee89 <kung_fu_fighting_monkey@hotmail.com>
2024-01-03 15:19:41 +0000

    refactor cors

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 from fastapi import FastAPI
 from routes.app import router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://skillshare-app.onrender.com/",
+    "http://localhost/",
+    "http://localhost:8080/",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=[""],
+    allow_headers=[""],
+)
 
 app.include_router(router)
