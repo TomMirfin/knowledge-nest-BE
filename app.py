78768f6a0236ba56907230dc3c0aa0d843cc124d
matt12171 <matthewpatterson@hotmail.co.uk>
2024-01-03 10:26:59 +0000

    Complete Articles

--- app.py
+++ app.py
@@ -90,7 +90,7 @@ async def show_user(id: str):
         raise HTTPException(status_code=404, detail='User not found')
 
 @app.delete("/users/{id}", response_description="Delete a user")
-async def delete_student(id: str):
+async def delete_user(id: str):
     try:
         delete_result = await user_collection.delete_one({"_id": ObjectId(id)})
     except Exception:
@@ -137,16 +137,26 @@ async def update_student(id: str, user: UpdateUserModel = Body(...)):
 
 # ------------------------------------------------------------------
 
+
+def get_current_timestamp():
+    return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+
 class ArticleModel(BaseModel):
     id: Optional[PyObjectId] = Field(alias="_id", default=None)
     username: str = Field(...)
     title: str = Field(...)
     topic: str = Field(...)
     body: str = Field(...)
-    created_at: Optional[str] = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    created_at: Optional[str] = Field(default_factory=get_current_timestamp)
+
+class UpdateArticleModel(BaseModel):
+    title: Optional[str] = None
+    topic: Optional[str] = None
+    body: Optional[str] = None
 
 class ArticleCollection(BaseModel):
-    users: List[ArticleModel]
+    articles: List[ArticleModel]
 
 @app.post('/articles',response_description="Add new articles",
     response_model=ArticleModel,
@@ -167,3 +177,68 @@ async def create_article(article: ArticleModel = Body(...)):
 
 async def list_articles():
     return ArticleCollection(articles=await article_collection.find().to_list(1000))
+
+@app.get('/articles/{id}',response_model=ArticleModel,
+    response_model_by_alias=False)
+
+
+async def show_article(id: str):
+    try:
+        article_id = ObjectId(id)
+    except InvalidId:
+        raise HTTPException(status_code=400, detail='Invalid id format')
+
+
+    try:
+        if (article := await article_collection.find_one({"_id": ObjectId(id)})) is not None:
+            return article
+        else:
+            raise HTTPException(status_code=404, detail='Article not found')
+    except Exception as err:
+        raise HTTPException(status_code=404, detail='Article not found')
+
+@app.delete("/articles/{id}", response_description="Delete an article")
+async def delete_article(id: str):
+    try:
+        delete_result = await article_collection.delete_one({"_id": ObjectId(id)})
+    except Exception:
+        raise HTTPException(status_code=400, detail='Invalid article format')
+
+    if delete_result.deleted_count == 1:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    raise HTTPException(status_code=404, detail=f"Article {id} not found")
+
+
+@app.put(
+    "/articles/{id}",
+    response_description="Update an article",
+    response_model=ArticleModel,
+    response_model_by_alias=False,
+)
+async def update_article(id: str, article: UpdateArticleModel = Body(...)):
+    article = {
+        k: v for k, v in article.model_dump(by_alias=True).items() if v is not None
+    }
+
+    if len(article) >= 1:
+        try:
+            update_result = await article_collection.find_one_and_update(
+                {"_id": ObjectId(id)},
+                {"$set": article},
+                return_document=ReturnDocument.AFTER,
+            )
+        except:
+            raise HTTPException(status_code=404, detail=f"Article id incorrect")
+        if update_result is not None:
+            return update_result
+        else:
+            raise HTTPException(status_code=404, detail=f"article {id} not found")
+    if len(article) == 0:
+        raise HTTPException(status_code=400, detail=f"Bad request")
+
+    if (existing_article := await article_collection.find_one({"_id": id})) is not None:
+        return existing_article
+ 
+    raise HTTPException(status_code=404, detail=f"article {id} not found")
+
